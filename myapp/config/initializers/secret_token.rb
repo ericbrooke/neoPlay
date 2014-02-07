3a3bbbaf302aed8c32db2b140bb85ee698d7a82e
Eric Brooke <ericbrooke@mac.com>
2014-02-06 21:33:42 -0800

    setup of rails Neo4J app

---
+++ myapp/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Myapp::Application.config.secret_key_base = '2e51683ea473604517dafaa94f10c470b2c7107e223ee246758fc052ac7459ea8d40ac2c48a2137b087bc0f975c0cd2afcd29a65a4282c926231dc6e458e9ce5'
